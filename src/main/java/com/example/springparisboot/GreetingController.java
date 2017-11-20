f4699360464b8f6b0b677c90a143e12575eaf435
erwanbornier <ebornier@pivotal.io>
2017-11-20 16:48:18 +0100

    update scripts

--- src/main/java/com/example/springparisboot/GreetingController.java
+++ src/main/java/com/example/springparisboot/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class GreetingController {
@@ -13,10 +14,13 @@ public class GreetingController {
 	@Autowired
 	CounterService counterService;
 
+        @Value("${vcap.application.space_name}")
+	String env; 
+
 	@RequestMapping("/")
 	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
        counterService.increment("spring.paris.attendees.view");
-			 model.addAttribute("name", name);
+			 model.addAttribute("name", env);
 			 return "greeting";
 	}
 
